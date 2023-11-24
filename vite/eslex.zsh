8cae0381b6bbb07f3cf4df985335de27453c22b7
ayii <a0913873707@gmail.com>
2023-11-24 19:49:10 +0800

    更新

---
+++ vite/eslex.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# eslex 別名
+# 可用來將專案中的 eslint 設定檔配置，直接覆蓋到一個統一的設定檔中
+cp ./eslint.config.mjs $HOME/UserConfig/config/eslint.config.mjs
---
+++ vite/eslim.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# eslim 腳本
+# 可以將統一的 eslint 設定檔覆蓋到專案中
+cp $HOME/UserConfig/config/eslint.config.mjs ./eslint.config.mjs

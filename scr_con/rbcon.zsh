a4daf552d5f429b98a995359bbfb834dfb72926d
ayii <a0913873707@gmail.com>
2024-11-10 19:57:41 +0800

    最後重製

--- scr_con/rbcon.zsh
+++ scr_con/rbcon.zsh
@@ -5,12 +5,16 @@
 local basePath=$HOME/UserConfig
 
 # zshrc
-cp $basePath/zsh_config/.zshrc $HOME/.zshrc
+# cp $basePath/zsh_config/.zshrc $HOME/.zshrc
 
-# vsCode(Cursor)
-cp $basePath/vsCode/settings.json "$HOME/Library/Application Support/Cursor/User/settings.json"
-cp $basePath/vsCode/keybindings.json "$HOME/Library/Application Support/Cursor/User/keybindings.json"
-cp $basePath/vsCode/snippets "$HOME/Library/Application Support/Cursor/User/snippets"
+# Cursor
+# cp $basePath/vsCode/settings.json "$HOME/Library/Application Support/Cursor/User/settings.json"
+# cp $basePath/vsCode/keybindings.json "$HOME/Library/Application Support/Cursor/User/keybindings.json"
+# cp $basePath/vsCode/snippets "$HOME/Library/Application Support/Cursor/User/snippets"
+
+cp $basePath/vsCode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+cp $basePath/vsCode/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
+cp $basePath/vsCode/snippets "$HOME/Library/Application Support/Code/User/snippets"
 
 cp $basePath/config/Rime/bopomo_onion.schema.yaml "$HOME/Library/Rime/bopomo_onion.schema.yaml"
 cp $basePath/config/Rime/default.custom.yaml "$HOME/Library/Rime/default.custom.yaml"
